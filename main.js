b5da686098765ac90fe71583b8abcb83178e4c64
Ahmed Abdou <hakam4all@yahoo.com>
2023-06-20 00:16:35 -0700

    Commit 20 June Progress

--- main.js
+++ main.js
@@ -1,82 +1,50 @@
-booking1.style.display = 'none';
-booking2.style.display = 'none';
-booking3.style.display = 'none';
-booking4.style.display = 'none';
+stepOne.style.display = 'none';
+secondBooking.style.display = 'none';
 
 
-// Step 1 - Basic Information
+// Step 1 
 let x = 0;
 step1.addEventListener('click', function (event) {
     event.preventDefault();
-    booking2.style.display = 'none';
-    booking3.style.display = 'none';
-    booking4.style.display = 'none';
+    secondBooking.style.display = 'none';
 
     if (x === 0) {
-        booking1.style.display = 'none';
+        stepOne.style.display = 'none';
         x = 1;
     } else {
-        booking1.style.display = 'grid';
+        stepOne.style.display = 'grid';
         x = 0;
     }
 });
 
 next1.addEventListener('click', function (event) {
     event.preventDefault();
-    let numberOfPeople = Number(people.value);
-    let bookingDate = bookDate.value;
-    booking1.style.display = 'none';
-    booking2.style.display = 'grid';
+    // let numberOfPeople = Number(people.value);
+    // let bookingDate = bookDate.value;
+    stepOne.style.display = 'none';
+    secondBooking.style.display = 'grid';
 
 });
 
-// Step 2 - Building
+// Step 2 
 let y = 0;
 step2.addEventListener('click', function (event) {
     event.preventDefault();
-    booking1.style.display = 'none';
-    booking3.style.display = 'none';
-    booking4.style.display = 'none';
+    stepOne.style.display = 'none';
 
     if (y === 0) {
-        booking2.style.display = 'none';
+        secondBooking.style.display = 'none';
         y = 1;
     } else {
-        booking2.style.display = 'grid';
+        secondBooking.style.display = 'grid';
         y = 0;
     }
 });
 
 next2.addEventListener('click', function (event) {
     event.preventDefault();
-    let numberOfPeople = Number(people2.value);
-    let bookingDate = bookDate2.value;
-    booking2.style.display = 'none';
-    booking3.style.display = 'block';
+    // let numberOfPeople = Number(people2.value);
+    // let bookingDate = bookDate2.value;
+    secondBooking.style.display = 'none';
 });
 
-
-// Step 2 - Avaliable Selection
-let z = 0;
-step3.addEventListener('click', function (event) {
-    event.preventDefault();
-    booking1.style.display = 'none';
-    booking2.style.display = 'none';
-    booking4.style.display = 'none';
-
-    if (z === 0) {
-        booking3.style.display = 'none';
-        z = 1;
-    } else {
-        booking3.style.display = 'block';
-        z = 0;
-    }
-});
-
-next3.addEventListener('click', function (event) {
-    event.preventDefault();
-    let numberOfPeople = Number(people3.value);
-    let bookingDate = bookDate3.value;
-    booking3.style.display = 'none';
-    booking4.style.display = 'grid';
-});
